7e9f4ea5cca27185af6252d1b049c5541d2cac1e
Stanley Chiemela Paul <stanleypaul313@gmail.com>
2022-12-21 14:29:33 -0800

    Reviewed 5 files

--- string1.c
+++ string1.c
@@ -1,12 +1,13 @@
 #include "shell.h"
 
 /**
- * _strcpy - copies a string
- * @dest: the destination
- * @src: the source
- *
- * Return: pointer to destination
- */
+* _strcpy - copies a string
+* @dest: the destination
+* @src: the source
+*
+* Return: pointer to destination
+*/
+
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
@@ -23,11 +24,12 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
- * _strdup - duplicates a string
- * @str: the string to duplicate
- *
- * Return: pointer to the duplicated string
- */
+* _strdup - duplicates a string
+* @str: the string to duplicate
+*
+* Return: pointer to the duplicated string
+*/
+
 char *_strdup(const char *str)
 {
 	int length = 0;
@@ -46,11 +48,11 @@ char *_strdup(const char *str)
 }
 
 /**
- * _puts - prints an input string
- * @str: the string to be printed
- *
- * Return: Nothing
- */
+* _puts - prints an input string
+* @str: the string to be printed
+*
+* Return: Nothing
+*/
 void _puts(char *str)
 {
 	int i = 0;
@@ -65,12 +67,13 @@ void _puts(char *str)
 }
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+
 int _putchar(char c)
 {
 	static int i;
--- tokenizer.c
+++ tokenizer.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- * **strtow - splits a string into words. Repeat delimiters are ignored
- * @str: the input string
- * @d: the delimeter string
- * Return: a pointer to an array of strings, or NULL on failure
- */
+* **strtow - splits a string into words. Repeat delimiters are ignored
+* @str: the input string
+* @d: the delimeter string
+* Return: a pointer to an array of strings, or NULL on failure
+*/
 
 char **strtow(char *str, char *d)
 {
@@ -49,11 +49,11 @@ char **strtow(char *str, char *d)
 }
 
 /**
- * **strtow2 - splits a string into words
- * @str: the input string
- * @d: the delimeter
- * Return: a pointer to an array of strings, or NULL on failure
- */
+* **strtow2 - splits a string into words
+* @str: the input string
+* @d: the delimeter
+* Return: a pointer to an array of strings, or NULL on failure
+*/
 char **strtow2(char *str, char d)
 {
 	int i, j, k, m, numwords = 0;
